e00dfcea8ab9b2d5758c7865d1ef349fdeb30699
francesco-sodano <frsodano@github.com>
2021-10-13 10:55:46 +0200

    Updated Deployment command

--- arm-template/deployment.sh
+++ arm-template/deployment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-az deployment group create --name Birdy --resource-group "rg-demo-birdy" --template-uri https://raw.githubusercontent.com/francesco-sodano/birdy/main/arm-template/azuredeploy.json --parameters https://raw.githubusercontent.com/francesco-sodano/birdy/main/arm-template/azuredeploy.parameters.json --rollback-on-error
+az deployment group create --name Birdy --resource-group "rg-demo-birdy" --template-uri https://raw.githubusercontent.com/francesco-sodano/birdy/main/arm-template/azuredeploy.json --parameters https://raw.githubusercontent.com/francesco-sodano/birdy/main/arm-template/azuredeploy.parameters.json
