85df443730371b81b77db6c6219150e5bbbf54b4
francesco-sodano <francesco-sodano@github.com>
2022-08-07 23:28:11 +0200

    Updated names and references

---
+++ device/software/birdy/birdy_controller.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Project Birdy - v202110.1
+# Copyright 2021 - Francesco Ares Sodano
+
+import os
+import json
+from pijuice import PiJuice
+from gpiozero import CPUTemperature
+from datetime import datetime
+from time import sleep
+from azure.iot.device.aio import IoTHubDeviceClient, IoTHubModuleClient
+from azure.iot.device import Message
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.core.exceptions import AzureError, ResourceExistsError
+
+def main():
+    # Importing settings
+    try:
+        configFile = f'{os.path.dirname(os.getcwd())}/birdy_config.json'
+        with open(configFile, "rt") as file:
+            deviceConfig = json.load(file)
+            print ("File loaded")
+    except:
+        result = {"statusCode": 400, "statusDescription" : "Error loading configuration"}
+        print ("Error loading configuration")
+        return result
