8a73d72c1ad0dc022d568435d034e019dfb621e9
francesco-sodano <francesco-sodano@github.com>
2022-09-11 23:26:59 +0200

    Updated version of the tools

--- tools/sensor_testers/camera_tester.py
+++ tools/sensor_testers/camera_tester.py
@@ -10,7 +10,7 @@ def main():
         # Initializing Camera - asking for square picture
         deviceCamera = Picamera2()
         deviceCameraConfig = deviceCamera.create_still_configuration()
-        deviceCamera.still_configuration.size = (800,800)
+        deviceCamera.still_configuration.size = (1280,1280)
         deviceCamera.configure(deviceCameraConfig)
         print("Camera initialized")
     except:
@@ -20,7 +20,7 @@ def main():
         return result
     try:
         # Taking set of pictures
-        deviceCamera.start_and_capture_files("test{:d}.jpeg", initial_delay=1, delay=1, num_files=10, show_preview=False)
+        deviceCamera.start_and_capture_files("test{:d}.jpeg", initial_delay=1, delay=1, num_files=2, show_preview=False)
     except:
         result = {"statusCode": 400, "statusDescription" : "Camera issue on taking photo"}
         # Camera error in taking picture
